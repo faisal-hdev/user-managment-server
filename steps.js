c547c5b9f3aebec447952c6998d9dcb31e575f51
faisal-hdev <itsfaisalchowdhury@gmail.com>
2024-04-20 17:10:05 +0600

    user sever file push

---
+++ steps.js
@@ -0,0 +1,9 @@
+/**
+ * 1. create a post api on the server side
+ * 2. client side sent data via post
+ * 3. set fetch method inside the fetch option (second parameter)
+ * 4. options will have three things: methods
+ * 5. options will have headers: 'content-type':'application/json'
+ * 6. dont forget to sand data by JSON.stringify in the body
+ * 7. on the server side: dont forget to use express.json() middleware
+ */
